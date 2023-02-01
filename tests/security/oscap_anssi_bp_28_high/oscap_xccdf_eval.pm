b626f51479fef39c0c5fa2bca8113c3b0921c5ea
Viktors Trubovics <viktors.trubovics@suse.com>
2023-02-01 15:05:02 +0000

    hipaa, anssi_bp_28_high profile tests

--- tests/security/oscap_anssi_bp_28_high/oscap_xccdf_eval.pm
+++ tests/security/oscap_anssi_bp_28_high/oscap_xccdf_eval.pm
@@ -1,7 +1,7 @@
 # Copyright 2022 SUSE LLC
 # SPDX-License-Identifier: GPL-2.0-or-later
 #
-# Summary: Test 'cis' hardening in the 'scap-security-guide': detection mode
+# Summary: Test 'anssi_bp_28_high' hardening in the 'scap-security-guide': detection mode
 # Maintainer: QE Security <none@suse.de>
 # Tags: poo#93886, poo#104943
 
@@ -19,15 +19,15 @@ sub run {
 
     # Get ds file and profile ID
     my $f_ssg_ds = is_sle ? $oscap_tests::f_ssg_sle_ds : $oscap_tests::f_ssg_tw_ds;
-    my $profile_ID = is_sle ? $oscap_tests::profile_ID_sle_cis : $oscap_tests::profile_ID_tw;
-    my $n_passed_rules = 210;
+    my $profile_ID = is_sle ? $oscap_tests::profile_ID_sle_anssi_bp_28_high : $oscap_tests::profile_ID_tw;
+    my $n_passed_rules = 97;
     my $n_failed_rules = 5;
 
     if (is_s390x) {
-        $n_passed_rules = 209;
+        $n_passed_rules = 97;
         $n_failed_rules = 5;
     }
-    my $eval_match = 'content_rule_is_fips_mode_enabled,content_rule_partition_for_var_log_audit,content_rule_smartcard_pam_enabled,content_rule_grub2_password,content_rule_no_files_unowned_by_user';
+my $eval_match = 'content_rule_rpm_verify_permissions,content_rule_install_hids,content_rule_accounts_passwords_pam_faillock_deny,content_rule_accounts_passwords_pam_faillock_unlock_time,content_rule_accounts_password_pam_lcredit';
 
     $self->oscap_evaluate($f_ssg_ds, $profile_ID, $n_passed_rules, $n_failed_rules, $eval_match);
 }
--- tests/security/oscap_stig/oscap_xccdf_eval.pm
+++ tests/security/oscap_stig/oscap_xccdf_eval.pm
@@ -27,12 +27,7 @@ sub run {
         $n_passed_rules = 209;
         $n_failed_rules = 5;
     }
-    my $eval_match = 'm/
-                    Rule.*content_rule_is_fips_mode_enabled.*Result.*fail.*
-                    Rule.*content_rule_partition_for_var_log_audit.*Result.*fail.*
-                    Rule.*content_rule_smartcard_pam_enabled.*Result.*fail.*
-                    Rule.*content_rule_grub2_password.*Result.*fail.*
-                    Rule.*content_rule_no_files_unowned_by_user.*Result.*fail/sxx';
+    my $eval_match = 'content_rule_is_fips_mode_enabled,content_rule_partition_for_var_log_audit,content_rule_smartcard_pam_enabled,content_rule_grub2_password,content_rule_no_files_unowned_by_user';
 
     $self->oscap_evaluate($f_ssg_ds, $profile_ID, $n_passed_rules, $n_failed_rules, $eval_match);
 }
