97de7aef0493f9eca478a6232a9fd2620121001f
Viktors Trubovics <viktors.trubovics@suse.com>
2023-02-07 08:48:19 +0000

    Expected for arm fix

--- tests/security/oscap_stig/oscap_xccdf_eval.pm
+++ tests/security/oscap_stig/oscap_xccdf_eval.pm
@@ -22,27 +22,26 @@ sub run {
     my $profile_ID = is_sle ? $oscap_tests::profile_ID_sle_stig : $oscap_tests::profile_ID_tw;
     my $n_passed_rules = 210;
     my $n_failed_rules = 5;
+    my @eval_match = (
+    'content_rule_is_fips_mode_enabled',
+    'content_rule_partition_for_var_log_audit',
+    'content_rule_smartcard_pam_enabled',
+    'content_rule_grub2_password',
+    'content_rule_no_files_unowned_by_user');
 
     if (is_s390x) {
         $n_passed_rules = 209;
         $n_failed_rules = 5;
     }
-
+    # Exclusion for ARM platform
     if (is_aarch64) {
         my @eval_match = (
-        'content_rule_is_fips_mode_enabled', 
+        'content_rule_is_fips_mode_enabled',
         'content_rule_partition_for_var_log_audit',
-        'content_rule_smartcard_pam_enabled', 
-        'content_rule_grub2_uefi_password', 
+        'content_rule_smartcard_pam_enabled',
+        'content_rule_grub2_uefi_password',
         'content_rule_no_files_unowned_by_user');
     }
-    
-    my @eval_match = (
-    'content_rule_is_fips_mode_enabled', 
-    'content_rule_partition_for_var_log_audit',
-    'content_rule_smartcard_pam_enabled', 
-    'content_rule_grub2_password', 
-    'content_rule_no_files_unowned_by_user');
 
     $self->oscap_evaluate($f_ssg_ds, $profile_ID, $n_passed_rules, $n_failed_rules, \@eval_match);
 }
