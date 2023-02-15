de12b5abf8c72058c3e9e62d8553084f5c0c55d6
Viktors Trubovics <viktors.trubovics@suse.com>
2023-02-15 17:07:48 +0000

    pcm fix

--- lib/oscap_tests.pm
+++ lib/oscap_tests.pm
@@ -263,8 +263,12 @@ sub oscap_security_guide_setup {
             # Package'ansible' require PackageHub is available
             return unless is_phub_ready();
             add_suseconnect_product(get_addon_fullname('phub'));
-            # add_suseconnect_product('sle-module-public-cloud') if (is_sle('<15'));
-            script_run("suseconnect -p sle-module-public-cloud/12/x86_64") if (is_sle('<15'));
+            add_suseconnect_product(get_addon_fullname('pcm'), (is_sle('<15') ? '12' : undef);
+            # if (is_sle('>15')) {
+                # my $major_version = '$(echo ${VERSION_ID}|cut -c1-2)';
+                # my $arch = get_var('ARCH');
+                # script_run("suseconnect -p sle-module-public-cloud/$major_version/$arch") ;
+            # }
         }
         zypper_call "in $pkgs sudo";
         # Record the pkgs' version for reference
