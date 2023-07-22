df8580b7f2caf730209d5d98fe4e750ed4f43625
Viktors Trubovics <viktors.trubovics@suse.com>
2023-07-22 16:10:34 +0100

    modified python3 cmd

--- lib/oscap_tests.pm
+++ lib/oscap_tests.pm
@@ -462,8 +462,10 @@ sub generate_mising_rules {
     my $profile = $_[1];
     my $output_file = "missing_rules.txt";
 
-    my $cmd = "python3 $compliance_as_code_path/build-scripts/profile_tool.py stats --missing --skip-stats --profile $profile --benchmark $f_ssg_sle_xccdf --format plain > $output_file";
-    assert_script_run("$cmd");
+    my $cmd = "python3 $compliance_as_code_path/build-scripts/profile_tool.py stats --missing --skip-stats --profile $profile --benchmark $f_ssg_sle_xccdf --format plain";
+    # assert_script_run("$cmd");
+    my $output = script_output("$cmd");
+    assert_script_run("echo $output > $output_file");
     record_info("Generated file $output_file", "generate_mising_rules Input file $f_ssg_sle_xccdf/n Command:\n$cmd");
     my $output_full_path = script_output("pwd");
     $output_full_path =~ s/\r|\n//g;
