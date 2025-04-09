f747d21391eecc9d94d11f6ef0424379d5d9ec81
Viktors Trubovics <viktors.trubovics@suse.com>
2025-04-09 18:54:23 +0100

    more fixes

--- tests/security/eal4/dbus_polkit_authorization.pm
+++ tests/security/eal4/dbus_polkit_authorization.pm
@@ -26,30 +26,27 @@ sub run {
     # Start DBus monitoring in background
     my $monitor_pid = background_script_run("dbus-monitor --system 'interface=org.freedesktop.PolicyKit1.Authority' > /tmp/dbus-monitor.log");
 
-    # # Create test user (if needed - OpenQA usually has a dedicated test user)
-    # assert_script_run('useradd -m -g users test_user') unless script_run('id test_user') == 0;
-
     # Attempt to set time as unprivileged user (should fail)
     select_console('user-console');
     my $unpriv_output = script_output(
         qq{dbus-send --system --print-reply --dest="org.freedesktop.timedate1" } .
         qq{/org/freedesktop/timedate1 org.freedesktop.timedate1.SetTime } .
         qq{int64:$test_time boolean:true boolean:true 2>&1},
-        proceed_on_failure => 1
+        proceed_on_failure => 1,
+        timeout => 30
     );
 
-    # Verify unprivileged attempt was denied
-    if ($unpriv_output !~ /org\.freedesktop\.DBus\.Error\.InteractiveAuthorizationRequired/) {
-        record_info("Unpriv. attempt", $unpriv_output, result => 'fail');
+    # Verify output
+    if ($unpriv_output =~ /org\.freedesktop\.DBus\.Error\.InteractiveAuthorizationRequired/) {
+        record_info("Success", "Polkit correctly denied access", result => 'ok');
+    } else {
         $self->result('fail');
+        record_info("Failure", "Unexpected output: $unpriv_output", result => 'fail');
+        save_screenshot;
+        return;
     }
-    # validate_script_output 'virsh net-list --all | grep default', qr/default\s+inactive\s+no\s+yes/;
-    # validate_script_output("dbus-send --system --print-reply --dest=\"org.freedesktop.timedate1\" /org/freedesktop/timedate1 org.freedesktop.timedate1.SetTime int64:$test_time boolean:true boolean:true", sub { m/org\.freedesktop\.DBus\.Error\.InteractiveAuthorizationRequired/ });
 
-    # die "Polkit did not deny unprivileged access" 
-        # unless $unpriv_output =~ /org\.freedesktop\.DBus\.Error\.InteractiveAuthorizationRequired/;
-
-    # Attempt to set time as root (should succeed)
+   # Attempt to set time as root (should succeed)
     select_console('root-console');
     my $root_output = script_output(
         qq{dbus-send --system --print-reply --dest="org.freedesktop.timedate1" } .
@@ -65,8 +62,12 @@ sub run {
         );
         $self->result('fail');
     }
-    # die "Root access to SetTime failed" 
-        # unless $root_output =~ /method return/;
+    else {
+        record_info(
+            "Root access pass",
+            "Got uccessful time setting",
+        );
+    }
 
     # Stop DBus monitoring and check logs
     script_run("kill $monitor_pid");
@@ -82,11 +83,16 @@ sub run {
         );
         $self->result('fail');
     }
+    else {
+        record_info(
+            "DBus monitor pass",
+            "Found Polkit traces in monitor log",
+        );
+    }
     # upload_log_file("/tmp/dbus-monitor.log");
     upload_logs("/tmp/dbus-monitor.log", timeout => 600);
 
     # Cleanup
-    # assert_script_run('userdel -r test_user');
     assert_script_run('rm -f /tmp/dbus-monitor.log');
 }
 
