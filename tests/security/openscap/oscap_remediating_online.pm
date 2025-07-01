91d1a45422824b2dd98f288c2cc7d18e975d05a5
Viktors Trubovics <viktors.trubovics@suse.com>
2025-07-01 18:23:33 +0100

    fix

--- tests/security/openscap/oscap_remediating_online.pm
+++ tests/security/openscap/oscap_remediating_online.pm
@@ -28,25 +28,26 @@ sub run {
     }, timeout => 300;
 
     validate_file_content($remediate_result);
-    if (validate_script_output "cat $remediate_result", sub {
-            qr{
-                version="[0-9]+\.[0-9]+"\s+encoding="UTF-8"
-                <Benchmark.*<Profile\s+id="standard"
-                select.*no_direct_root_logins.*selected="true"
-                select.*rule_misc_sysrq.*selected="true"
-                Rule.*no_direct_root_logins"\s+selected="false"
-                Rule.*rule_misc_sysrq"\s+selected="false"
-                TestResult.*platform.*cpe:\/o:suse
-                rule-result.*no_direct_root_logins.*\s+.*result.*fixed
-                rule-result.*rule_misc_sysrq.*\s+.*result.*fixed
-                score\s+system="urn:xccdf:scoring:default"
-                maximum="[0-9]+}sx
-        }, timeout => 300)
+    if ($remediate_result_content =~ qr{
+        version="[0-9]+\.[0-9]+"\s+encoding="UTF-8"
+        .*?<Benchmark.*?<Profile\s+id="standard"
+        .*?select.*?no_direct_root_logins.*?selected="true"
+        .*?select.*?rule_misc_sysrq.*?selected="true"
+        .*?Rule.*?no_direct_root_logins".*?selected="false"
+        .*?Rule.*?rule_misc_sysrq".*?selected="false"
+        .*?TestResult.*?platform.*?cpe:\/o:suse
+        rule-result idref="no_direct_root_logins.*?result.*?fixed
+        rule-result idref="rule_misc_sysrq.*?result.*?fixed
+        .*?score\s+system="urn:xccdf:scoring:default".*?maximum="[0-9]+"
+        }sx) 
+    {
+        record_info("Remediate eval passed", "scap online remediation output check passed");
     }
-    else {
-        record_soft_failure('bsc#1245559 - Open SCAP 1.3.7.+ changed remediation functionality: 2 test rules in the xccdf are not fixied - became [notapplicable]');
+    else
+    {
+        record_soft_failure('bsc#1245559 - Open SCAP 1.3.7.+ remediation check failed. Possible issues: XML structure changed or rules not fixed.');
+        $self->result('fail');
     }
-
     # Verify the remediate action result
     if (script_run "! [[ -e /etc/securetty ]]") {
         validate_script_output "cat /etc/securetty", sub { m/^$/ };
