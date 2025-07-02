9f373ba88b99514d16dd7578c2bde1d674bf6fb3
Viktors Trubovics <viktors.trubovics@suse.com>
2025-07-02 16:49:39 +0100

    extra checks

--- tests/security/openscap/oscap_remediating_offline.pm
+++ tests/security/openscap/oscap_remediating_offline.pm
@@ -18,34 +18,63 @@ sub run {
     ensure_generated_file($xccdf_result);
     prepare_remediate_validation;
 
-    validate_script_output "oscap xccdf remediate --results $remediate_result $xccdf_result", sub {
-        qr/
-              Rule.*no_direct_root_logins.*Result.*fixed.*
-              Rule.*rule_misc_sysrq.*Result.*fixed/sxx
-    }, timeout => 300;
+    my $offline_rem_out = validate_script_output "oscap xccdf remediate --results $remediate_result $xccdf_result", timeout => 300;
+    if ($offline_rem_out =~ qr/
+        Rule.*no_direct_root_logins.*Result.*fixed.*
+        Rule.*rule_misc_sysrq.*Result.*fixed/sx) {
+        record_info("Offline remediate passed", "scap remediation output check passed");
+    }
+    else {
+        record_info("Remediate eval failed", "scap online remediation output check failed", result => 'fail');
+        record_soft_failure('bsc#1245559 - Open SCAP 1.3.7.+ remediation check failed. Possible issues: XML structure changed or rules not fixed.');
+    }
 
     validate_file_content($remediate_result);
-    validate_script_output "cat $remediate_result", sub {
-        qr/
-            <\?xml\s+version="[0-9]+\.[0-9]+"\s+encoding="UTF-8".*
-            <Benchmark.*<Profile\s+id="standard".*
-            select.*no_direct_root_logins.*selected="true".*
-            select.*rule_misc_sysrq.*selected="true".*
-            Rule.*no_direct_root_logins"\s+selected="false".*
-            Rule.*rule_misc_sysrq"\s+selected="false".*
-            TestResult.*
-            rule-result.*idref="no_direct_root_logins".*result.*fail.*
-            rule-result.*idref="rule_misc_sysrq".*result.*fail.*
-            TestResult.*
-            rule-result.*idref="no_direct_root_logins".*result.*fixed.*
-            rule-result.*idref="rule_misc_sysrq".*result.*fixed.*
-            score\s+system="urn:xccdf:scoring:default".*
-            maximum="[0-9]+/sxx
-    }, timeout => 300;
+    my $remediate_result_out = script_output "cat $remediate_result");
+    if ($remediate_result_out =~ qr/
+        <\?xml\s+version="[0-9]+\.[0-9]+"\s+encoding="UTF-8".*
+        <Benchmark.*<Profile\s+id="standard".*
+        select.*no_direct_root_logins.*selected="true".*
+        select.*rule_misc_sysrq.*selected="true".*
+        Rule.*no_direct_root_logins"\s+selected="false".*
+        Rule.*rule_misc_sysrq"\s+selected="false".*
+        TestResult.*
+        rule-result.*idref="no_direct_root_logins".*result.*fail.*
+        rule-result.*idref="rule_misc_sysrq".*result.*fail.*
+        TestResult.*
+        rule-result.*idref="no_direct_root_logins".*result.*fixed.*
+        rule-result.*idref="rule_misc_sysrq".*result.*fixed.*
+        score\s+system="urn:xccdf:scoring:default".*
+        maximum="[0-9]+/sx) {
+        record_info("Check Passed", "Remediation result check passed");
+    }
+    else {
+        record_info("Check failed", "Remediation result check faled", result => 'fail');
+        record_soft_failure('bsc#1245559 - Open SCAP 1.3.7.+ remediation check failed. Possible issues: XML structure changed or rules not fixed.');
+    }
 
     # Verify the remediate action result
-    validate_script_output "cat /etc/securetty", sub { m/^$/ };
-    validate_script_output "cat /proc/sys/kernel/sysrq", sub { m/^0$/ };
+    if (script_run "! [[ -e /etc/securetty ]]") {
+        if (validate_script_output "cat /etc/securetty", sub { m/^$/ }) {
+            record_info("Check passed", "/etc/securetty exists");
+        }
+        else {
+            record_info("Check failed", "/etc/securetty does not contain 0", result => 'fail');
+            record_soft_failure('bsc#1245559 - Open SCAP 1.3.7.+ changed remediation functionality: 2 test rules in the xccdf are not fixied - became [notapplicable]');
+        }
+    }
+    else {
+        record_info("Check failed", "/etc/securetty does not exist", result => 'fail');
+        record_soft_failure('bsc#1245559 - Open SCAP 1.3.7.+ changed remediation functionality: 2 test rules in the xccdf are not fixied - became [notapplicable]');
+    }
+
+    if (script_run('grep -e \'^0$\' /proc/sys/kernel/sysrq') == 0, timeout => 30) {
+        record_info("sysrq eval passed", "/proc/sys/kernel/sysrq contains 0");
+    }
+    else {
+        record_info("Check failed", "/proc/sys/kernel/sysrq do not contain 0", result => 'fail');
+        record_soft_failure('bsc#1245559 - Open SCAP 1.3.7.+ changed remediation functionality: 2 test rules in the xccdf are not fixied - became [notapplicable]');
+    }
 
     # Restore
     finish_remediate_validation;
--- tests/security/openscap/oscap_remediating_online.pm
+++ tests/security/openscap/oscap_remediating_online.pm
@@ -18,14 +18,6 @@ sub run {
     prepare_remediate_validation;
 
     # Remediate
-    # validate_script_output "oscap xccdf eval --remediate --profile standard --results $remediate_result xccdf.xml", sub {
-        # qr/
-            # Rule.*no_direct_root_logins.*Result.*fail.*
-            # Rule.*rule_misc_sysrq.*Result.*fail.*
-            # Starting\s+Remediation.*
-            # Rule.*no_direct_root_logins.*Result.*fixed.*
-            # Rule.*rule_misc_sysrq.*Result.*fixed/sxx
-    # }, timeout => 300;
     my $eval_output = script_output "oscap xccdf eval --remediate --profile standard --results $remediate_result xccdf.xml";
     if ($eval_output =~ qr/
         Rule.*no_direct_root_logins.*Result.*fail.*
@@ -55,21 +47,30 @@ sub run {
         record_info("Remediate eval passed", "scap online remediation output check passed");
     }
     else {
+        record_info("Remediate eval faled", "scap online remediation output check faled");
         record_soft_failure('bsc#1245559 - Open SCAP 1.3.7.+ remediation check failed. Possible issues: XML structure changed or rules not fixed.');
         # result('fail');
     }
     # Verify the remediate action result
     if (script_run "! [[ -e /etc/securetty ]]") {
-        validate_script_output "cat /etc/securetty", sub { m/^$/ };
+        if (validate_script_output "cat /etc/securetty", sub { m/^$/ }) {
+            record_info("Check passed", "/etc/securetty exists");
+        }
+        else {
+            record_info("Check failed", "/etc/securetty does not contain 0", result => 'fail');
+            record_soft_failure('bsc#1245559 - Open SCAP 1.3.7.+ changed remediation functionality: 2 test rules in the xccdf are not fixied - became [notapplicable]');
+        }
     }
     else {
+        record_info("Check failed", "/etc/securetty does not exist", result => 'fail');
         record_soft_failure('bsc#1245559 - Open SCAP 1.3.7.+ changed remediation functionality: 2 test rules in the xccdf are not fixied - became [notapplicable]');
     }
-    # validate_script_output "cat /proc/sys/kernel/sysrq", sub { m/^0$/ };
+
     if (script_run('grep -e \'^0$\' /proc/sys/kernel/sysrq') == 0, timeout => 30) {
         record_info("sysrq eval passed", "/proc/sys/kernel/sysrq contains 0");
     }
     else {
+        record_info("Check failed", "/proc/sys/kernel/sysrq do not contain 0", result => 'fail');
         record_soft_failure('bsc#1245559 - Open SCAP 1.3.7.+ changed remediation functionality: 2 test rules in the xccdf are not fixied - became [notapplicable]');
     }
 
