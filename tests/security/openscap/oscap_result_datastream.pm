54268b73b8fd7fce5578ecb02c00456dde297d2d
Viktor <viktors.trubovics@suse.com>
2025-07-10 16:45:28 +0100

    more fixes

--- tests/security/openscap/oscap_result_datastream.pm
+++ tests/security/openscap/oscap_result_datastream.pm
@@ -18,24 +18,25 @@ sub run {
     assert_script_run "oscap xccdf eval --results-arf $arf_result $source_ds";
 
     validate_file_content($arf_result);
-    validate_script_output "cat $arf_result", sub {
-        qr{
-            version="[0-9]+\.[0-9]+"\s+encoding="UTF-8".*
-            <arf:asset-report-collection.*xmlns:core
-            <ns\d:criteria\s+operator="AND".*
-            test_ref="oval:no_direct_root_logins:tst:1.*
-            test_ref="oval:etc_securetty_exists:tst:2.*
-            <ns\d:criterion.*test_ref="oval:rule_misc_sysrq:tst:1".*
-            <arf:reports.*<arf:report.*<arf:content.*<oval_results.*
-            <oval:product_name>cpe:\/a:open-scap:oscap.*
-            <test\s+test_id="oval:etc_securetty_exists:tst:2".*
-            check="all"\s+result="not\sevaluated".*
-            <test\s+test_id="oval:no_direct_root_logins:tst:1".*
-            check="all"\s+result="not\sevaluated".*
-            <test\s+test_id="oval:rule_misc_sysrq:tst:1".*
-            check="at\sleast\sone"\s+result="not\sevaluated".*
-            \/arf:asset-report-collection>}sx
-    }, timeout => 300;
+    my $arf_result_out = script_output "cat $arf_result";
+    my @arf_result_regex_list = (
+        qr/version="[0-9]+\.[0-9]+"\s+encoding="UTF-8".*/s,
+        qr/<arf:asset-report-collection.*xmlns:core/s,
+        qr/<ns\d:criteria\s+operator="AND".*/s,
+        qr/test_ref="oval:no_direct_root_logins:tst:1.*/s,
+        qr/test_ref="oval:etc_securetty_exists:tst:2.*/s,
+        qr/<ns\d:criterion.*test_ref="oval:rule_misc_sysrq:tst:1".*/s,
+        qr/<arf:reports.*<arf:report.*<arf:content.*<oval_results.*/s,
+        qr/<oval:product_name>cpe:\/a:open-scap:oscap.*/s,
+        qr/<test\s+test_id="oval:etc_securetty_exists:tst:2".*/s,
+        qr/check="all"\s+result="not evaluated".*/s,
+        qr/<test\s+test_id="oval:no_direct_root_logins:tst:1".*/s,
+        qr/check="all"\s+result="not evaluated".*/s,
+        qr/<test\s+test_id="oval:rule_misc_sysrq:tst:1".*/s,
+        qr/check="at least one"\s+result="not evaluated".*/s,
+        qr/\/arf:asset-report-collection>/s
+    );
+    validate_file_content_regex ($arf_result_out, \@arf_result_regex_list, $arf_result);
 }
 
 1;
--- tests/security/openscap/oscap_xccdf_scanning.pm
+++ tests/security/openscap/oscap_xccdf_scanning.pm
@@ -16,52 +16,56 @@ use version_utils qw(is_sle is_leap);
 
 sub run {
     # Always return failed here, so we use "||true" as workaround
-    validate_script_output "oscap xccdf eval --profile standard --results $xccdf_result xccdf.xml || true", sub {
-        qr/
-            Rule.*no_direct_root_logins.*fail.*
-            Rule.*rule_misc_sysrq.*fail/sxx;
-    };
+    my $xccdf_eval_out = script_output "oscap xccdf eval --profile standard --results $xccdf_result xccdf.xml || true";
+    my @xccdf_eval_regex_list = (
+        qr/Rule.*no_direct_root_logins.*fail/s,
+        qr/Rule.*rule_misc_sysrq.*fail/s
+    );
+    validate_file_content_regex ($xccdf_eval_out, \@xccdf_eval_regex_list, "oscap xccdf eval");
+
     validate_file_content($xccdf_result);
-    validate_script_output "cat $xccdf_result", sub {
-        qr/
-            encoding="UTF-8".*
-            <Benchmark.*<Profile\s+id="standard".*<select.*
-            idref="no_direct_root_logins"\ selected="true".*
-            idref="rule_misc_sysrq"\s+selected="true".*
-            <Rule\s+id="no_direct_root_logins"\s+selected="false".*
-            <Rule\s+id="rule_misc_sysrq"\s+selected="false".*
-            <TestResult.*<platform.*cpe:\/o:(open)?suse.*
-            <rule-result.*idref="no_direct_root_logins".*<result.*fail.*
-            <rule-result.*idref="rule_misc_sysrq".*<result.*fail.*
-            <score\s+system="urn:xccdf:scoring:default".*
-            maximum="[0-9]+/sxx
-    }, timeout => 300;
+    my $xccdf_result_out = script_output "cat $xccdf_result";
+    my @xccdf_result_regex_list = (
+        qr/encoding="UTF-8".*/s,
+        qr/<Benchmark.*<Profile\s+id="standard".*<select.*/s,
+        qr/idref="no_direct_root_logins"\s+selected="true".*/s,
+        qr/idref="rule_misc_sysrq"\s+selected="true".*/s,
+        qr/<Rule\s+id="no_direct_root_logins"\s+selected="false".*/s,
+        qr/<Rule\s+id="rule_misc_sysrq"\s+selected="false".*/s,
+        qr/<TestResult.*<platform.*cpe:\/o:(open)?suse.*/s,
+        qr/<rule-result.*idref="no_direct_root_logins".*<result.*fail.*/s,
+        qr/<rule-result.*idref="rule_misc_sysrq".*<result.*fail.*/s,
+        qr/<score\s+system="urn:xccdf:scoring:default".*/s,
+        qr/maximum="[0-9]+/s
+    );
+    validate_file_content_regex ($xccdf_result_out, \@xccdf_result_regex_list, $xccdf_result);
+
     # Single rule testing only available on the higher version for
     # openscap-utils
     if (!(is_sle('<15') or is_leap('<15.0'))) {    # openscap >= 1.2.16
-        validate_script_output "oscap xccdf eval --profile standard --rule no_direct_root_logins --results $xccdf_result_single xccdf.xml || true",
-          sub {
-            qr/
-                Title.*Direct\ root\ Logins\ Not\ Allowed.*
-                Rule.*no_direct_root_logins.*fail/sxx
-          };
+        my $xccdf_eval_single_out = script_output "oscap xccdf eval --profile standard --rule no_direct_root_logins --results $xccdf_result_single xccdf.xml || true";
+        my @xccdf_regex_list = (
+            qr/Title.*Direct root Logins Not Allowed/s,
+            qr/Rule.*no_direct_root_logins.*fail/s
+        );
+        validate_file_content_regex ($xccdf_eval_single_out, \@xccdf_regex_list, "oscap xccdf eval --profile standard --rule no_direct_root_logins");
 
         validate_file_content($xccdf_result_single);
-        validate_script_output "cat $xccdf_result_single", sub {
-            qr/
-                encoding="UTF-8".*
-                <Benchmark.*<Profile\s+id="standard".*<select.*
-                idref="no_direct_root_logins"\ selected="true".*
-                idref="rule_misc_sysrq"\s+selected="true".*
-                <Rule\s+id="no_direct_root_logins"\s+selected="false".*
-                <Rule\s+id="rule_misc_sysrq"\s+selected="false".*
-                <TestResult.*<platform.*cpe:\/o:(open)?suse.*
-                <rule-result.*idref="no_direct_root_logins".*<result.*fail.*
-                <rule-result.*idref="rule_misc_sysrq".*<result.*notselected.*
-                <score\s+system="urn:xccdf:scoring:default".*
-                maximum="[0-9]+/sxx
-        }, timeout => 300;
-
+        my $xccdf_result_single_out = script_output "cat $xccdf_result_single";
+        my @xccdf_result_single_regex_list = (
+            qr/encoding="UTF-8".*/s,
+            qr/<Benchmark.*<Profile\s+id="standard".*<select.*/s,
+            qr/idref="no_direct_root_logins"\s+selected="true".*/s,
+            qr/idref="rule_misc_sysrq"\s+selected="true".*/s,
+            qr/<Rule\s+id="no_direct_root_logins"\s+selected="false".*/s,
+            qr/<Rule\s+id="rule_misc_sysrq"\s+selected="false".*/s,
+            qr/<TestResult.*<platform.*cpe:\/o:(open)?suse.*/s,
+            qr/<rule-result.*idref="no_direct_root_logins".*<result.*fail.*/s,
+            qr/<rule-result.*idref="rule_misc_sysrq".*<result.*notselected.*/s,
+            qr/<score\s+system="urn:xccdf:scoring:default".*/s,
+            qr/maximum="[0-9]+/s
+        );
+        validate_file_content_regex ($xccdf_result_single_out, \@xccdf_result_single_regex_list, $xccdf_result_single);
     }
 }
 
